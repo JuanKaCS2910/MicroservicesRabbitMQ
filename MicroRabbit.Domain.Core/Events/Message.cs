2e58dfc1f5510e79baf30c16210ed9723ffd2200
Juan Carlos <juan.castro.socla@gmail.com>
2023-09-02 23:49:19 -0500

    Add project files.

---
+++ MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+	public abstract class Message : IRequest<bool>
+	{
+
+		public string MessageType { get; protected set; }
+
+		protected Message()
+		{
+			//El GetType hace referencia al nombre de la clase que está ejecutando este Message.
+			MessageType = GetType().Name;
+		}
+
+	}
+}
