052357fe62c7be5c05da58d6c021b64f462a2290
Juan Carlos <juan.castro.socla@gmail.com>
2023-09-03 22:34:32 -0500

    Inyección de Dependencias.

--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +17,13 @@ namespace MicroRabbit.Infra.IoC
 			//Domain Bus
 			services.AddTransient<IEventBus, RabbitMQBus>();
 			services.Configure<RabbitMQSettings>(c => configuration.GetSection("RabbitMQSettings"));
+
+			//Application Service
+			services.AddTransient<IAccountService, AccountService>();
+
+			//Data
+			services.AddTransient<IAccountRepository, AccountRepository>();
+			services.AddTransient<BankingDbContext>();
 		}
 
 	}
