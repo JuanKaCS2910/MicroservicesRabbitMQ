b1e961d49bfa9c9c91f75fa821531a3271cf4d51
Juan Carlos <juan.castro.socla@gmail.com>
2023-09-06 00:18:49 -0500

    Migraciones  - Creacion de Controlles - Mediator

---
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+	[Route("api/[controller]")]
+	public class BankingController : ControllerBase
+	{
+		private readonly IAccountRepository _accountRepository;
+
+		public BankingController(IAccountRepository accountRepository)
+		{
+			_accountRepository = accountRepository;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Account>> Get()
+		{
+			return Ok(_accountRepository.GetAccounts());
+		}
+	}
+}
--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,4 +1,5 @@
-﻿using MicroRabbit.Banking.Application.Interfaces;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
@@ -7,13 +8,18 @@ using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace MicroRabbit.Infra.IoC
 {
-	public  class DependencyContainer
+	public class DependencyContainer
 	{
 		public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
-		{
+		{ 
+			//MediatR Mediator
+			services.AddMediatR(Assembly.GetExecutingAssembly());
+
+
 			//Domain Bus
 			services.AddTransient<IEventBus, RabbitMQBus>();
 			services.Configure<RabbitMQSettings>(c => configuration.GetSection("RabbitMQSettings"));
