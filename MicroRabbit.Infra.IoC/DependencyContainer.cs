ac8e8333202ae2d38ee5de163afd906a32ca2fb5
Juan Carlos <juan.castro.socla@gmail.com>
2023-09-13 00:29:41 -0500

    Ejecutar Microservice Consumer.

--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,18 +1,6 @@
 ﻿using MediatR;
-using MicroRabbit.Banking.Application.Interfaces;
-using MicroRabbit.Banking.Application.Services;
-using MicroRabbit.Banking.Data.Context;
-using MicroRabbit.Banking.Data.Repository;
-using MicroRabbit.Banking.Domain.CommandHandlers;
-using MicroRabbit.Banking.Domain.Commands;
-using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
-using MicroRabbit.Transfer.Application.Interfaces;
-using MicroRabbit.Transfer.Application.Services;
-using MicroRabbit.Transfer.Data.Context;
-using MicroRabbit.Transfer.Data.Repository;
-using MicroRabbit.Transfer.Domain.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -24,7 +12,7 @@ namespace MicroRabbit.Infra.IoC
 		public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
 		{
 
-			services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
+			//services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
 
 			//MediatR Mediator
 			services.AddMediatR(Assembly.GetExecutingAssembly());
@@ -32,19 +20,18 @@ namespace MicroRabbit.Infra.IoC
 
 			//Domain Bus
 			services.AddTransient<IEventBus, RabbitMQBus>();
-			//services.Configure<RabbitMQSettings>(c => configuration.GetSection("RabbitMQSettings"));
 
 			//Application Service
-			services.AddTransient<IAccountService, AccountService>();
-			services.AddTransient<ITransferService, TransferService>();
+			//services.AddTransient<IAccountService, AccountService>();
+			//services.AddTransient<ITransferService, TransferService>();
 
 			//Data
-			services.AddTransient<IAccountRepository, AccountRepository>();
-			services.AddTransient<ITransferRepository, TransferRepository>();
+			//services.AddTransient<IAccountRepository, AccountRepository>();
+			//services.AddTransient<ITransferRepository, TransferRepository>();
 
 			//Context
-			services.AddTransient<BankingDbContext>();
-			services.AddTransient<TransferDbContext>();
+			//services.AddTransient<BankingDbContext>();
+			//services.AddTransient<TransferDbContext>();
 
 			return services;
 		}
